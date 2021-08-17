76f1da2ebb721ed4cf96e59fd17545b0b1337bbb
Yves Findlay <YvesFindlay@Yvess-MBP.lan>
2021-08-17 01:51:20 +0100

    Added bridge and Mountain SVG's to landscape, modified Moon svg position

---
+++ src/components/SVG/Grass.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './Grass.scss';
+
+const Grass = () => {
+  return (
+    <svg className="intro__grass" width="1469" height="100" viewBox="0 0 1469 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M154.818 61.0617L1469 34.0016V122.002H0V60.2441L154.818 61.0617Z" fill="#1B1B2D"/>
+    
+    </svg>
+  );
+}
+
+export default Grass;
+
+
+

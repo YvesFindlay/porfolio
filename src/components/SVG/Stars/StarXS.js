3c9479299b78c7a7cc69515d917bbe99e4680c55
Yves Findlay <YvesFindlay@Yvess-MBP.lan>
2021-08-19 21:20:43 +0100

    Added folder for each respective svg, better code maintenance

---
+++ src/components/SVG/Stars/StarXS.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Star from './Star';
+
+const StarXS = () => (
+    <React.Fragment>
+        <Star size='intro__star--xs' pos='intro__star--t42-l8' />
+        <Star size='intro__star--xs' pos='intro__star--t16-r7' />  
+        <Star size='intro__star--xs' pos='intro__star--t33-r40' />
+        <Star size='intro__star--xs' pos='intro__star--t6-r2' />
+        <Star size='intro__star--xs' pos='intro__star--t48-r15' />
+        <Star size='intro__star--xs' pos='intro__star--t40-r12' />
+        <Star size='intro__star--xs' pos='intro__star--t20-l14' />
+        <Star size='intro__star--xs' pos='intro__star--t48-r15' />
+        <Star size='intro__star--xs' pos='intro__star--t23-r43' />
+    </React.Fragment>
+)
+
+export default StarXS;

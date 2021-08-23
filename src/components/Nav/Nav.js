5eb1c588653ee8a41ae17538dd407b6d7228d57d
Yves Findlay <YvesFindlay@Yvess-MBP.lan>
2021-08-23 16:04:50 +0100

    changed color of nav when side drawer is open

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,28 +4,31 @@ import ToggleNavButton from '../SideDrawerNav/ToggleNavButton';
 
 import './Nav.scss';
 
-const Nav = (props) => (
-    <header className="nav">
-        <nav className="nav__navigation">
-        <div className="nav__logo"> <a href="/">YF</a> </div>
-        <div className="nav__spacer"/>
-            <div className="nav__hamburger">
-                <ToggleNavButton onClick={props.onSaveDrawerState} />
-            </div>
-           
-            {/* <div className="nav__navigation-items">
-                <ul> 
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/">About</a></li>
-                    <li><a href="/">Contact</a></li>
-                </ul>
-            </div> */}
-            
-        </nav>
-    </header>
+const Nav = (props) => {
+    let navClasses = 'nav' 
+    navClasses += props.drawerState ? " nav--transparent" : " nav--dark-bg";
 
-);
+    return (
+        <header className={navClasses}>
+            <nav className="nav__navigation">
+            <div className="nav__logo"> <a href="/">YF</a> </div>
+            <div className="nav__spacer"/>
+                <div className="nav__hamburger">
+                    <ToggleNavButton onClick={props.onSaveDrawerState} />
+                </div>
+        
+                {/* <div className="nav__navigation-items">
+                    <ul> 
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/">About</a></li>
+                        <li><a href="/">Contact</a></li>
+                    </ul>
+                </div> */}
+                
+            </nav>
+        </header>
+    )
 
-
+}
 
 export default Nav;

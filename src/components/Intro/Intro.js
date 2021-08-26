45d3a9062a8a274530a0e245c009fe52bf9752b1
Yves Findlay <YvesFindlay@Yvess-MBP.lan>
2021-08-26 14:23:28 +0100

    Stars are now rendered progrmatically
    Removed all occurences of manual rendering of stars (e.g multiple instances of the star component

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -4,40 +4,34 @@ import Moon from '../SVG/Moon/Moon';
 import Cloud from '../SVG/Cloud/Cloud';
 import Mountains from '../SVG/Mountains/Mountains';
 import Bridge from '../SVG/Bridge/Bridge';
-import StarXS from '../SVG/Stars/StarXS';
-import StarS from '../SVG/Stars/StarS';
-import StarM from '../SVG/Stars/StarM';
+import Star from '../SVG/Star/Star';
 import TrainLights from '../SVG/TrainLights/TrainLights';
 import Train from '../SVG/Train/Train';
 import TrainSign from '../SVG/TrainSign/TrainSign';
 
 import './Intro.scss';
 
-const Sky = () => {    
+const Sky = () => (
+    <main className="intro">
+        <Moon/>
+        <Star />
+        <Cloud size='intro__cloud--xs' pos='cloud1' cloudID={1} />
+        <Cloud size='intro__cloud--s' pos='cloud2' cloudID={2} />
+        <Cloud size='intro__cloud--m' pos='cloud3' cloudID={3} />
+        <Cloud size='intro__cloud--s' pos='cloud4' cloudID={4} />
+        <Cloud size='intro__cloud--xs' pos='cloud5' cloudID={5} /> 
+        <Cloud size='intro__cloud--m' pos='cloud6' cloudID={6} /> 
+        <Cloud size='intro__cloud--s' pos='cloud9' cloudID={9} /> 
+        <Cloud size='intro__cloud--m' pos='cloud10' cloudID={10} /> 
+        <Cloud size='intro__triple-cloud--m' pos='triple-cloud' cloudID='triple' />
+        <Mountains />
+        <Bridge> 
+            <Train />
+            <TrainSign />
+        </Bridge>
+        <TrainLights />
+    </main>
+)
 
-    return (
-        <main className="intro">
-            <Moon/>
-            <StarXS />
-            <StarS />
-            <StarM />
-            <Cloud size='intro__cloud--xs' pos='cloud1' cloudID={1} />
-            <Cloud size='intro__cloud--s' pos='cloud2' cloudID={2} />
-            <Cloud size='intro__cloud--m' pos='cloud3' cloudID={3} />
-            <Cloud size='intro__cloud--s' pos='cloud4' cloudID={4} />
-            <Cloud size='intro__cloud--xs' pos='cloud5' cloudID={5} /> 
-            <Cloud size='intro__cloud--m' pos='cloud6' cloudID={6} /> 
-            <Cloud size='intro__cloud--s' pos='cloud9' cloudID={9} /> 
-            <Cloud size='intro__cloud--m' pos='cloud10' cloudID={10} /> 
-            <Cloud size='intro__triple-cloud--m' pos='triple-cloud' cloudID='triple' />
-            <Mountains />
-            <Bridge> 
-              <Train/>
-              <TrainSign />
-            </Bridge>
-            <TrainLights />
-        </main>
-    )
-}
     
 export default Sky;

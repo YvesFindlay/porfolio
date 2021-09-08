59a96729ddd21bcce8803b909cdd443c52e0e120
Yves Findlay <YvesFindlay@Yvess-MBP.lan>
2021-09-08 15:32:34 +0100

    lifted state up for (gsap) nav animation
    Also included implementation of reverse animation to initial nav hamburger state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Nav from './components/Nav/Nav';
 import SideDrawerNav from './components/SideDrawerNav/SideDrawerNav';
 import Backdrop from './components/Backdrop/Backdrop';
-import Intro from './components/Intro/Intro'
+import Intro from './components/Intro/Intro';
+// import About from './components/Sections/About/About';
+import AboutGrid from './components/Sections/About/AboutGrid';
+import { gsap } from 'gsap';
 
 import './App.scss';
 
-
 const App = () => {
+
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const [navAnimation, setNavAnimation] = useState({})
+
   const drawerClickHandler = () => {
     setIsDrawerOpen((prevState) => !prevState);
   }
@@ -19,16 +24,50 @@ const App = () => {
 
   let backDrop;
 
-  if(isDrawerOpen){
-    backDrop = <Backdrop onClick={backDropClickHandler} />;
+  if (isDrawerOpen) {
+    backDrop = <Backdrop nav={navAnimation} onClickBackdrop={backDropClickHandler} />
   }
+  
+  const navClickAnimationTo = ({width, pos, navLine1, navLine2, navLine3}) => {
+    if(!gsap) return;
+    let navLn1Pos = pos;
+    let navLn3Pos = pos + 1;
+    
+    const navTl = gsap.timeline({
+      repeat: 0,
+    });
+
+    navTl.fromTo(navLine1.current, {width: navLine1.current.style.width}, {width: '100%'})
+    .fromTo(navLine3.current,  {width: navLine3.current.style.width}, {width: '100%'}, "<")
+    .to(navLine1.current, {y: navLn1Pos})
+    .to(navLine3.current, {y: -navLn3Pos}, "<",)
+    .to(navLine2.current, {opacity: 0}, "-=0.8") // -=0.58
+    .to(navLine1.current, {rotate: -43, transformOrigin: 'center'}, 0.7)
+    .to(navLine3.current, {rotate: 43, transformOrigin: 'center'}, 0.7).totalDuration(1);
+
+    setNavAnimation({ 
+      navTl,
+      navLine1, 
+      navLine2, 
+      navLine3,
+      width, 
+      pos
+    });
+  }
+
+ 
 
   return (
     <div className="app"> 
-      <Nav onSaveDrawerState={drawerClickHandler} drawerState={isDrawerOpen}/>
+      <Nav 
+       onSaveDrawerState={drawerClickHandler} 
+       drawerState={isDrawerOpen}
+       onAnimateNav={navClickAnimationTo}
+      />
       <SideDrawerNav display={isDrawerOpen} />
       {backDrop}
       <Intro />
+      <AboutGrid />
     </div>
 
   );
--- src/components/Backdrop/Backdrop.js
+++ src/components/Backdrop/Backdrop.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import { gsap } from 'gsap';
 
 import './Backdrop.scss';
 
-const Backdrop = (props) => <div className="backdrop" onClick={props.onClick}/>;
+const Backdrop = ({nav, onClickBackdrop}) => {
+
+    const {navLine1, navLine2, navLine3} = nav;
+
+    const BackdropCallbackHandler = () => {
+
+        const navReverseTl = gsap.timeline({
+            repeat: 0,
+        });
+
+        navReverseTl.to(navLine3.current, {rotate: 0, transformOrigin: 'center'}, 0.05)
+        .to(navLine1.current, {rotate: 0, transformOrigin: 'center'},"<")
+        .to(navLine2.current, {opacity: 1}, 0.5)
+        .to(navLine3.current, {y: 0}, )
+        .to(navLine1.current, {y: 0}, "<")
+        .to(navLine1.current, {width: '50%'})
+        .to(navLine3.current, {width: '50%'}, "<").totalDuration(0.8);     
+        
+        onClickBackdrop();
+    }
+
+    return (
+        <div className="backdrop" onClick={BackdropCallbackHandler} />
+    )
+}
 
 export default Backdrop;

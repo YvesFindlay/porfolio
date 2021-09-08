a25622c86197b867cf1e77f798c3db4c7abf876a
Yves Findlay <YvesFindlay@Yvess-MBP.lan>
2021-09-08 15:24:23 +0100

    Added ScrollDown component
    Component for notfifying this site is scroll based, and enables motion design

---
+++ src/components/ScrollDown/ScrollDown.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './ScrollDown.scss';
+
+const ScrollDown = () => (
+    <div className="scroll-down__container">
+        <div className="scroll-down">
+        <span className="scroll-down__title"> Scroll down</span>
+        </div>
+    </div>
+)
+
+
+export default ScrollDown;

c75e49e54853f7fe285398c916c3a0c4a21e852c
Yves Findlay <YvesFindlay@Yvess-MBP.lan>
2021-09-20 13:53:51 +0100

    Added scroll animation for stars and Train

---
+++ src/HelperFunctions/scrollAnimation.js
@@ -0,0 +1,27 @@
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+ const scrollAnimation = ({ pos, ref, ease, start, end, trigger, endTrigger, stagger, scrub, onUpdate }) => {
+  
+  gsap.registerPlugin(ScrollTrigger);
+
+  gsap.config({
+    nullTargetWarn: false,
+  });
+
+  gsap.to(ref, {
+    [pos]: (_, el) => (parseFloat(el.getAttribute("data-speed"))) * ScrollTrigger.maxScroll(window),
+    ease,
+    stagger,
+    scrollTrigger: {
+      start,
+      trigger,
+      endTrigger,
+      end,
+      scrub,
+      onUpdate,
+    }
+  });
+}
+
+export default scrollAnimation;

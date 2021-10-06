2dddafadbd493627701b71e2e7705653aeda15d1
Yves Findlay <YvesFindlay@Yvess-MBP.lan>
2021-10-06 13:28:08 +0100

    Added remaining sections to app component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Nav from './components/Nav/Nav';
 import SideDrawerNav from './components/SideDrawerNav/SideDrawerNav';
 import Backdrop from './components/Backdrop/Backdrop';
 import Intro from './components/Intro/Intro';
 // import About from './components/Sections/About/About';
 import AboutGrid from './components/Sections/About/AboutGrid';
+import Skills from './components/Sections/Skills/Skills';
+import Projects from './components/Sections/Projects/Projects';
+import Contact from './components/Sections/Contact/Contact';
+import Location from './components/Sections/Location/Location';
+
+import UseWindowSize from './HelperFunctions/UseWindowSize';
 import { gsap } from 'gsap';
 
 import './App.scss';
 
 const App = () => {
 
+  // const getInnerWidth = (width) => {
+  //   return window.innerWidth < width;
+  // }
+  // const imgDisplayFlag = getInnerWidth.bind(this, 600);
+
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
   const [wasClicked, setWasClicked] = useState(false);
-  const [navAnimation, setNavAnimation] = useState({})
+  const [navAnimation, setNavAnimation] = useState({});
 
   const navClickAnimation = ({pos, navLine1, navLine2, navLine3}) => {
     setWasClicked(true);
@@ -67,6 +78,26 @@ const App = () => {
     backDrop = <Backdrop nav={navAnimation} onClickBackdrop={backDropClickHandler} />
   }
 
+
+  // useEffect(() => {
+  //   const handleResize = () => {
+  //     setIsMobile(
+  //       imgDisplayFlag()
+  //     );
+  //   }
+      
+  //   window.addEventListener('resize', handleResize);
+    
+  //   return () => {
+  //     window.removeEventListener('resize', handleResize);
+  //   }
+
+  // });
+ 
+  // console.log(imgDisplayFlag());
+
+  let dimensions = UseWindowSize(600);
+
   return (
     <div className="app"> 
       <Nav 
@@ -74,11 +105,15 @@ const App = () => {
        drawerState={isDrawerOpen}
        onAnimateNav={navClickAnimation}
        wasClicked={wasClicked}
-      />
+      /> 
       <SideDrawerNav display={isDrawerOpen} />
       {backDrop}
       <Intro />
-      <AboutGrid />
+      <AboutGrid isMobile={dimensions.isMobile} />
+      <Skills />
+      <Projects />
+      <Location />
+      <Contact />
     </div>
   );
 }

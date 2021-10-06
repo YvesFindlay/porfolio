6bcf312c27a601f7c36fe59415200e1a50d474f5
Yves Findlay <YvesFindlay@Yvess-MBP.lan>
2021-10-06 13:21:58 +0100

    Added skills infinite carousel component

--- src/components/Sections/Skills/Skills.js
+++ src/components/Sections/Skills/Skills.js
@@ -6,49 +6,38 @@ import { gsap, Power1 } from 'gsap';
 import './Skills.scss'
 
 const Skills = () => {
-    // const skillsDOM = useRef([]);
-    // https://greensock.com/forums/topic/16036-animate-after/
+  useEffect(() => {
+    gsap.set('.skills__wrapper',{xPercent:-50,yPercent:-50})
 
-    useEffect(() => {
-      gsap.set('.skills__wrapper',{xPercent:-50,yPercent:-50})
-
-      const boxWidth = 180;
-      const totalWidth = boxWidth * 10;  //  * n of boxes
-      const no01 = document.querySelectorAll("#no01 .skills")
-      const fromLeft = "-=" + totalWidth;
+    const boxWidth = 180;
+    const totalWidth = boxWidth * 10;  //  * n of boxes
+    const no01 = document.querySelectorAll("#no01 .skills__item")
+    const fromLeft = "-=" + totalWidth;
           
-      const mod = gsap.utils.wrap(0, totalWidth);
+    const mod = gsap.utils.wrap(0, totalWidth);
         
-      const marquee = (which, time, direction) => {
-        gsap.set(which, {
-          x:(i) => {
-            return i * boxWidth;
-          }
-        });
+    const marquee = (which, time, direction) => {
+      gsap.set(which, {
+        x:(i) => {
+          return i * boxWidth;
+        }
+      });
 
-        const slide = gsap.timeline().to(which,  {
-          x: direction,
-          modifiers: {
-            x: x => mod(Number.parseFloat(x)) + "px"
-          },
+      const slide = gsap.timeline().to(which,  {
+        x: direction,
+        modifiers: {
+          x: x => mod(Number.parseFloat(x)) + "px"
+        },
         
-          duration:time, ease:'none',
-            repeat:-1,
-        });
+        duration:time, ease:'none', repeat:-1,
+      });
 
-        return slide;
-      } 
-
-
-    // gsap.registerPlugin(CSSRulePlugin);
-
-    // const rule = CSSRulePlugin.getRule(".skills__wrapper::before");
-
-
+      return slide;
+    } 
     // look into using bind method here to preset all repeating arguments
 
-     expandLineAnimation({
-      selector: ".skills__wrapper::before",
+    expandLineAnimation({
+      selector: ".skills .skills__wrapper::before",
       trigger: ".skills__wrapper",
       endTrigger: ".skills__wrapper",
       start: "-=100px center",
@@ -57,34 +46,35 @@ const Skills = () => {
     })
 
     expandLineAnimation({
-      selector: ".skills__wrapper::after",
+      selector: ".skills .skills__wrapper::after",
       trigger: ".skills__wrapper",
       endTrigger: ".skills__wrapper",
       start: "-=100px center",
       end: "bottom center",
       transformOrigin: "right",
       delay: 0.5,
-    }).from('.skills', {y:"+=100%", duration: 1, stagger: 0.1, ease: Power1.easeInOut}).add(marquee(no01, 10, fromLeft), ">");
-    }, []);
+    }).from('.skills__item', {y:"+=100%", duration: 0.6, stagger: 0.1, ease: Power1.easeInOut}).add(marquee(no01, 10, fromLeft), ">");
+  }, []);
 
-    return (
-      <Section sectionName="section__skills">
-        <div className="skills__wrapper">
-          <div id="no01" className="skills__container">
-            <div className="skills">A</div>
-            <div className="skills">B</div>
-            <div className="skills">C</div>
-            <div className="skills">D</div>
-            <div className="skills">E</div>
-            <div className="skills">F</div>
-            <div className="skills">G</div>
-            <div className="skills">H</div>
-            <div className="skills">I</div>
-            <div className="skills">J</div>
-          </div>
+  return (
+    // <Section sectionName="section__skills">
+    <section className="skills">
+      <div className="skills__wrapper">
+        <div id="no01" className="skills__container">
+          <div className="skills__item">A</div>
+          <div className="skills__item">B</div>
+          <div className="skills__item">C</div>
+          <div className="skills__item">D</div>
+          <div className="skills__item">E</div>
+          <div className="skills__item">F</div>
+          <div className="skills__item">G</div>
+          <div className="skills__item">H</div>
+          <div className="skills__item">I</div>
+          <div className="skills__item">J</div>
         </div>
-      </Section>
-    )
+      </div>
+    </section>
+  )
 }
 
 export default Skills;

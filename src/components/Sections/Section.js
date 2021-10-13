8df201e966ee3dce9f8a99482533e1355a9ac5eb
Yves Findlay <YvesFindlay@Yvess-MBP.lan>
2021-10-13 18:06:58 +0100

    removed unneeded section component

--- src/components/Sections/Section.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import './Section.scss'
-
-const Section = (props) => (
-    <section className={`${props.sectionName} card`}>{props.children}</section>
-)
-
-export default Section;

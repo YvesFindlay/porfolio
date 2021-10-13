7af5c349f57535b90546cb21c989e5486b5e9be6
Yves Findlay <YvesFindlay@Yvess-MBP.lan>
2021-10-13 20:39:13 +0100

    Added all individual skill items to skills component

--- src/components/Sections/Skills/Skills.js
+++ src/components/Sections/Skills/Skills.js
@@ -1,16 +1,22 @@
 import React, { useEffect } from 'react';
-import Section from '../Section';
 import expandLineAnimation from '../../../HelperFunctions/expandLineAnimation';
 import { gsap, Power1 } from 'gsap';
 
 import './Skills.scss'
 
 const Skills = () => {
+  const skills = ["JavaScript", "React", "HTML5", "Sass", "CSS3", "Bootstrap", "Figma", "GSAP3", "npm"];
+  const skillElems = skills.map((skill, ind)=> {
+    return (
+      <div key={ind} className="skills__item">{skill}</div>
+    )
+  })
+
   useEffect(() => {
     gsap.set('.skills__wrapper',{xPercent:-50,yPercent:-50})
-
-    const boxWidth = 180;
-    const totalWidth = boxWidth * 10;  //  * n of boxes
+    // on mobile change boxWidth based on viewport height
+    const boxWidth = 700;
+    const totalWidth = boxWidth * 9;  //  * n of boxes
     const no01 = document.querySelectorAll("#no01 .skills__item")
     const fromLeft = "-=" + totalWidth;
           
@@ -40,39 +46,31 @@ const Skills = () => {
       selector: ".skills .skills__wrapper::before",
       trigger: ".skills__wrapper",
       endTrigger: ".skills__wrapper",
-      start: "-=100px center",
+      start: "top center",
       end: "bottom center",
       transformOrigin: "left",
+      markers: true
     })
 
     expandLineAnimation({
       selector: ".skills .skills__wrapper::after",
       trigger: ".skills__wrapper",
       endTrigger: ".skills__wrapper",
-      start: "-=100px center",
+      start: "top center",
       end: "bottom center",
       transformOrigin: "right",
       delay: 0.5,
-    }).from('.skills__item', {y:"+=100%", duration: 0.6, stagger: 0.1, ease: Power1.easeInOut}).add(marquee(no01, 10, fromLeft), ">");
+    }).to('.skills__item', {opacity: 1, ease: Power1.easeInOut}, ">").add(marquee(no01, 20, fromLeft));
   }, []);
 
   return (
-    // <Section sectionName="section__skills">
     <section className="skills">
       <div className="skills__wrapper">
         <div id="no01" className="skills__container">
-          <div className="skills__item">A</div>
-          <div className="skills__item">B</div>
-          <div className="skills__item">C</div>
-          <div className="skills__item">D</div>
-          <div className="skills__item">E</div>
-          <div className="skills__item">F</div>
-          <div className="skills__item">G</div>
-          <div className="skills__item">H</div>
-          <div className="skills__item">I</div>
-          <div className="skills__item">J</div>
+          {skillElems}
         </div>
       </div>
+      <p>A list of skills I've acquired over the years</p>
     </section>
   )
 }

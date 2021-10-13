6a753c14a3133cc230a3100da8851e5dafcb6ba9
Yves Findlay <YvesFindlay@Yvess-MBP.lan>
2021-10-13 19:00:29 +0100

    Added text animation to Contact component, cleanup contact.scss

--- src/components/Sections/Contact/Contact.js
+++ src/components/Sections/Contact/Contact.js
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import Form from '../../Form/Form';
+import { createSplitText,  splitText } from '../../../HelperFunctions/textRevealAnimation';
 
 import './Contact.scss'
 
 const Contact = () => {
+
+    useEffect(()=> {
+        const h2Split = createSplitText({ 
+            el: ".contact__header", 
+            type: "lines", 
+            linesClass: "split-child" 
+        });
+
+        createSplitText({ 
+            el: ".contact__header", 
+            type: "lines", 
+            linesClass: "split-parent" 
+        });
+
+        splitText({
+            el: h2Split.lines,
+            stagger: 0.2,
+            trigger: ".contact",
+            endTrigger: ".contact",
+            start: "top center",
+            end: "bottom center",
+            markers: true
+        });
+
+    }, []);
+
     return (
         <section className="contact">
             <div className="contact__content">
-                <h2> Something in mind?</h2>
-                <h2> Get in touch </h2>
-                <p>Feel free to reach out if you wanna collaborate with me, or simply have a chat</p>
+                <h2 className="contact__header"> Something in mind?</h2>
+                <h2 className="contact__header"> Get in touch </h2>
+                <p className="contact__message">Feel free to reach out if you wanna collaborate with me, or simply have a chat</p>
             </div>
             <Form />
         </section>

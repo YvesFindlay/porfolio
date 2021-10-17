527f6799ac8fc30671ebd6a9672507f338677954
Yves Findlay <YvesFindlay@Yvess-MBP.lan>
2021-10-17 15:40:57 +0100

    Fixed cloud positioning and responsive sizing

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,7 +1,7 @@
 import React, { useRef } from "react";
 
 import Moon from "../SVG/Moon/Moon";
-// import Cloud from '../SVG/Cloud/Cloud';
+import Cloud from "../SVG/Cloud/Cloud";
 import Mountains from "../SVG/Mountains/Mountains";
 import Bridge from "../SVG/Bridge/Bridge";
 import Star from "../SVG/Star/Star";
@@ -18,14 +18,13 @@ const Sky = (props) => {
     <main className="intro">
       <Moon />
       <Star easing={props.easing} />
-      {/* <Cloud size='intro__cloud--xs' pos='cloud1' cloudID={1} />
-            <Cloud size='intro__cloud--s' pos='cloud2' cloudID={2} />
-            <Cloud size='intro__cloud--m' pos='cloud3' cloudID={3} />
-            <Cloud size='intro__cloud--s' pos='cloud4' cloudID={4} />
-            <Cloud size='intro__cloud--xs' pos='cloud5' cloudID={5} /> 
-            <Cloud size='intro__cloud--m' pos='cloud6' cloudID={6} /> 
-            <Cloud size='intro__cloud--s' pos='cloud9' cloudID={9} /> 
-            <Cloud size='intro__cloud--m' pos='cloud10' cloudID={10} />   */}
+      <Cloud size="intro__cloud--xs" pos="cloud1" cloudID={1} />
+      <Cloud size="intro__cloud--s" pos="cloud2" cloudID={2} />
+      <Cloud size="intro__cloud--m" pos="cloud3" cloudID={3} />
+      <Cloud size="intro__cloud--s" pos="cloud4" cloudID={4} />
+      <Cloud size="intro__cloud--xs" pos="cloud5" cloudID={5} />
+      <Cloud size="intro__cloud--m" pos="cloud6" cloudID={6} />
+      <Cloud size="intro__cloud--m" pos="cloud10" cloudID={10} />
       <Mountains />
       <TrainSign />
       <Bridge>
